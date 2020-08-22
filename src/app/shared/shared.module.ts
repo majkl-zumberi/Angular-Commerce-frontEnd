8b7879441ae15a8edf65330f6e95f465707e5340
majkl zumberi <majklzumberi00@gmail.com>
2020-08-22 20:21:02 +0200

    added shared module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  exports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ]
+})
+export class SharedModule { }
