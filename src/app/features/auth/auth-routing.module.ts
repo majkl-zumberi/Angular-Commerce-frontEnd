00052062afc1fb4f74655f743fcabec08d538968
majkl zumberi <majklzumberi00@gmail.com>
2020-08-22 17:48:22 +0200

    added login & sign-up routes under auth parent route

--- src/app/features/auth/auth-routing.module.ts
+++ src/app/features/auth/auth-routing.module.ts
@@ -2,8 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AuthComponent } from './components/main/auth.component';
+import {LoginComponent} from './components/login/login.component';
+import {SignUpComponent} from './components/sign-up/sign-up.component';
 
-const routes: Routes = [{ path: '', component: AuthComponent }];
+const routes: Routes = [
+  { path: '', redirectTo: '/auth/login', pathMatch: 'full' },
+  { path: '', component: AuthComponent, children : [
+      { path: 'login', component: LoginComponent},
+      { path: 'sign-up', component: SignUpComponent}
+    ] }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
