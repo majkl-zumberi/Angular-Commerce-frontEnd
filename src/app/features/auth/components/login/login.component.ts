2ff748fa499629c5c45df6ac79da08500c30ff7b
majkl zumberi <majklzumberi00@gmail.com>
2020-08-22 20:24:20 +0200

    handle form logic | boiler plate added animations fade & slide

--- src/app/features/auth/components/login/login.component.ts
+++ src/app/features/auth/components/login/login.component.ts
@@ -1,15 +1,57 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {Observable, Subscription} from 'rxjs';
+import {animate, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  animations: [
+    trigger('fade', [
+      transition('void => *', [
+        style({ opacity: 0 }),
+        animate(500, style({opacity: 1}))
+      ]),
+      transition('* => void', [
+        animate(200, style({opacity: 0}))
+      ])
+    ]),
+    trigger('slide', [
+      transition('void => *', [
+        style({transform: 'translateX(-100%)'}),
+        animate(700, style({transform: 'translateX(0)'}))
+      ])
+    ])
+  ]
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginError: string | null;
+  loginForm: FormGroup;
+  private sub: Subscription;
+  getState: Observable<any>;
+
+  get usernameControl(): FormControl {
+    return this.loginForm.get('username') as FormControl;
+  }
+  get passwordControl(): FormControl {
+    return this.loginForm.get('password') as FormControl;
+  }
+
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  loginUser() {
+    console.log('username:' + this.usernameControl.value);
+    console.log('password:' + this.passwordControl.value);
+  }
 }
--- src/app/features/auth/components/sign-up/sign-up.component.ts
+++ src/app/features/auth/components/sign-up/sign-up.component.ts
@@ -1,15 +1,71 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {AbstractControl, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {trigger, style, animate, transition} from '@angular/animations';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.scss']
+  styleUrls: ['./sign-up.component.scss'],
+  animations: [
+    trigger('fade', [
+      transition('void => *', [
+        style({ opacity: 0 }),
+        animate(500, style({opacity: 1}))
+      ]),
+      transition('* => void', [
+        animate(200, style({opacity: 0}))
+      ])
+    ]),
+    trigger('slide', [
+      transition('void => *', [
+        style({transform: 'translateX(-100%)'}),
+        animate(700, style({transform: 'translateX(0)'}))
+      ])
+    ])
+  ]
 })
-export class SignUpComponent implements OnInit {
+export class SignUpComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  passwordMessage: string;
+  signUpForm: FormGroup;
+  get usernameControl(): FormControl {
+    return this.signUpForm.get('email') as FormControl;
   }
 
+  constructor(private fb: FormBuilder) {
+    this.signUpForm = this.fb.group({
+      email: ['', Validators.required],
+      pswGroup: this.fb.group({
+        password: ['', Validators.required],
+        passwordCnf: ['', Validators.required],
+      }, {
+        validator: this.checkPasswords
+      })
+    });
+  }
+  registerUser() {
+    console.log('email:' + this.signUpForm.get('email').value);
+    console.log('password:' + this.signUpForm.get('pswGroup').get('password').value);
+  }
+
+  checkPasswords(c: AbstractControl): { [key: string]: boolean } | null {
+    const emailControl = c.get('password');
+    const confirmControl = c.get('passwordCnf');
+
+
+
+    if (emailControl.pristine || confirmControl.pristine) {
+      return null;
+    }
+
+    if (emailControl.value === confirmControl.value) {
+      return null;
+    }
+    return { match: true };
+  }
+
+  signUpUser() {
+    console.log('email:' + this.signUpForm.get('email').value);
+    console.log('password:' + this.signUpForm.get('pswGroup').get('password').value);
+  }
 }
