f7dba525345a08bf043feef533ca048f647e83da
majkl zumberi <majklzumberi00@gmail.com>
2020-08-23 22:17:18 +0200

    added reducer - login user failure action logic

--- src/app/redux/auth/auth.reducers.ts
+++ src/app/redux/auth/auth.reducers.ts
@@ -1,6 +1,6 @@
 import {User} from '../../core/model/user.interface';
 import {Action, createReducer, on} from '@ngrx/store';
-import {initUser} from './auth.actions';
+import {initUser, loginUserFailure} from './auth.actions';
 
 export interface UserState {
   user: User;
@@ -16,6 +16,8 @@ export const initialState: UserState = {
 export const authReducer = createReducer(
   initialState,
   on(initUser, (state, {user}) => ({ ...state, user, errorMessage: null })),
+  on(loginUserFailure, (state, {error}) => ({...state, user: null, errorMessage: error})),
+
 );
 
 export function reducer(state: UserState | undefined, action: Action) {
