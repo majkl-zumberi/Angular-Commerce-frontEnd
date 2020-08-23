f8e4409fe1b0d09c8d1777e23dc0e8c78e475853
majkl zumberi <majklzumberi00@gmail.com>
2020-08-23 22:16:25 +0200

    added effect - login actions

--- src/app/redux/auth/auth.actions.ts
+++ src/app/redux/auth/auth.actions.ts
@@ -2,3 +2,9 @@ import { createAction, props} from '@ngrx/store';
 import {User} from '../../core/model/user.interface';
 
 export const initUser  = createAction('[User] init',  props<{user: User}>());
+
+// effects
+// login
+export const loginUser = createAction('[Auth] Login',  props<{email: string, password: string}>());
+export const loginUserSuccess = createAction('[Auth] Login Success', props<{user: User}>());
+export const loginUserFailure = createAction('[Auth] Login Failure', props<{error: string}>());
