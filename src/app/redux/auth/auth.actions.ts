ae2acaed3e8b0c962119c760fffad82f12a3e587
majkl zumberi <majklzumberi00@gmail.com>
2020-08-24 18:23:26 +0200

    added signUp actions

--- src/app/redux/auth/auth.actions.ts
+++ src/app/redux/auth/auth.actions.ts
@@ -6,5 +6,11 @@ export const initUser  = createAction('[User] init',  props<{user: User}>());
 // effects
 // login
 export const loginUser = createAction('[Auth] Login',  props<{email: string, password: string}>());
-export const loginUserSuccess = createAction('[Auth] Login Success', props<{user: User}>());
+export const loginUserSuccess = createAction('[Auth] Login Success', props<{token: string}>());
 export const loginUserFailure = createAction('[Auth] Login Failure', props<{error: string}>());
+
+// signUp
+export const signUpUser = createAction('[Auth] signUp',  props<{email: string, password: string}>());
+export const signUpUserSuccess = createAction('[Auth] signUp Success', props<{token: string}>());
+export const signUpUserFailure = createAction('[Auth] signUp Failure', props<{error: string}>());
+
