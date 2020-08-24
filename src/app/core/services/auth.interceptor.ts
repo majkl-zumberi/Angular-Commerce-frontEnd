fc4a9a84ed903b132a290c97738ef5b20a2dd966
majkl zumberi <majklzumberi00@gmail.com>
2020-08-24 18:29:52 +0200

    added interceptor for handling requests | add a bearer token, if available

---
+++ src/app/core/services/auth.interceptor.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor() {}
+
+  intercept(req: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    const idToken = localStorage.getItem('token');
+
+    if (idToken) {
+      const cloned = req.clone({
+        headers: req.headers.set('Authorization',
+          'Bearer ' + idToken)
+      });
+
+      return next.handle(cloned);
+    } else {
+      return next.handle(req);
+    }
+  }
+}
