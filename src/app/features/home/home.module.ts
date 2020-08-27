ec49ab1b577f9de5c5332dee671a59a6e85fd941
majkl zumberi <majklzumberi00@gmail.com>
2020-08-27 20:35:26 +0200

    registered ClothesFacadeService as provider for home.module.ts

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -4,10 +4,12 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './components/main/home.component';
 import {SharedModule} from '../../shared/shared.module';
+import {ClothesFacadeService} from './components/services/clothes-facade.service';
 
 
 @NgModule({
   declarations: [HomeComponent],
+  providers: [ClothesFacadeService],
   imports: [
     CommonModule,
     HomeRoutingModule,
