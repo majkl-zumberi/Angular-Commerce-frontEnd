0d528ddc62cdf28e3c9ba8b03b71a997c720c301
majkl zumberi <majklzumberi00@gmail.com>
2020-08-27 20:33:30 +0200

    handled logic for retrieving all clothes using glide

--- src/app/features/home/components/main/home.component.ts
+++ src/app/features/home/components/main/home.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import Glide from '@glidejs/glide';
 import {DressImageLink} from '../../../../core/model/dressImageLink.interface';
 import {Dress} from '../../../../core/model/dress.interface';
+import {Store, select} from '@ngrx/store';
+import {retrieveAllClothes} from '../../../../redux/clothes/clothes.actions';
+import { selectClothes } from 'src/app/redux';
+import { Observable } from 'rxjs';
+import {ClothesFacadeService} from '../services/clothes-facade.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -11,34 +15,19 @@ export class HomeComponent implements OnInit {
 
   dress: Dress;
   links: DressImageLink[];
-  constructor() {
+
+  get clothesList(): Observable<Dress[]> {
+    return this.store.pipe(select(selectClothes));
+  }
+
+  constructor(private store: Store, private clotheFacadeService: ClothesFacadeService) {
   }
 
   ngOnInit(): void {
-    this.links = [
-      {
-        _id: 'sas',
-        link: 'https://images.unsplash.com/photo-1575436611232-44651b483d46?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1243&q=80'
-      },
-      {
-        _id: 'dod',
-        link: 'https://images.unsplash.com/photo-1550505364-441e12acc1dc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1234&q=80'
-      },
-      {
-        _id: 'lol',
-        link: 'https://images.unsplash.com/photo-1589905803031-9128fd807bbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1234&q=80'
-      }
-    ];
-    this.dress = {
-      _id: 'sooos',
-      name: 'wella',
-      price: 7.99,
-      links: this.links
-    };
-    const carousels = document.querySelectorAll('.glide');
-    carousels.forEach(carousel => {
-      new Glide(carousel).mount();
-    });
+    this.store.dispatch(retrieveAllClothes());
   }
 
+  showDetail(clothe: Dress) {
+    this.clotheFacadeService.goToDetail(clothe._id);
+  }
 }
