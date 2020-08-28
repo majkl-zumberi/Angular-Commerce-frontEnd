aa9550171e1ce46e824b5bd23a1e960dfcc0a60c
majkl zumberi <majklzumberi00@gmail.com>
2020-08-28 19:09:26 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: 'auth', loadChildren: () => import('./features/auth/auth.module').then(m => m.AuthModule) },
   { path: 'home', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) },
+  // tslint:disable-next-line:max-line-length
   { path: 'sessionExpired', loadChildren: () => import('./features/session-expired/session-expired.module').then(m => m.SessionExpiredModule) }];
 
 @NgModule({
