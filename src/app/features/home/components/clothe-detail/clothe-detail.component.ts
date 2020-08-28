274ef6fa0b9df790f806c57fd213537aa100d4ba
majkl zumberi <majklzumberi00@gmail.com>
2020-08-28 19:07:12 +0200

    reset form handled, now addToCart method will open a success modal| add's a component using component ref

--- src/app/features/home/components/clothe-detail/clothe-detail.component.ts
+++ src/app/features/home/components/clothe-detail/clothe-detail.component.ts
@@ -1,34 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ComponentFactoryResolver, ComponentRef, OnInit, ViewChild, ViewContainerRef} from '@angular/core';
 import {select, Store} from '@ngrx/store';
 import {Observable} from 'rxjs';
 import {Dress} from '../../../../core/model/dress.interface';
 import {getCurrentNavigatedClothe} from '../../../../redux';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {ActivatedRoute} from '@angular/router';
+import {SuccessModalComponent} from '../success-modal/success-modal.component';
 
 @Component({
   selector: 'app-clothe-detail',
   templateUrl: './clothe-detail.component.html',
-  styleUrls: ['./clothe-detail.component.scss']
+  styleUrls: ['./clothe-detail.component.scss'],
 })
 export class ClotheDetailComponent implements OnInit {
-
+  image: string;
   customizeForm: FormGroup;
 
   get clothe(): Observable<Dress> {
     return this.store.pipe(select(getCurrentNavigatedClothe));
   }
-  constructor(private fb: FormBuilder, private store: Store) {
-    this.customizeForm = this.fb.group({
-      color: ['', Validators.required],
+  @ViewChild('parent', {read: ViewContainerRef}) target: ViewContainerRef;
+  private componentRef: ComponentRef<any>;
+
+  constructor(private fb: FormBuilder,
+              private store: Store, private route: ActivatedRoute,
+              private componentFactoryResolver: ComponentFactoryResolver) {
+     this.image = this.route.snapshot.queryParamMap.get('img');
+     this.customizeForm = this.fb.group({
+      productColor: ['', Validators.required],
+      customText: [''],
+      textColor: [''],
     });
+     this.customizeForm.get('customText').valueChanges.subscribe(text => {
+      console.log(text);
+      if (text === '') {
+        this.customizeForm.get('textColor').disable();
+      } else {
+        this.customizeForm.get('textColor').enable();
+      }
+    });
+     this.customizeForm.get('textColor').disable();
   }
 
   ngOnInit(): void {
-    this.customizeForm.get('color').disable();
   }
 
   addToCart() {
     console.log('cliccato');
     console.log(this.customizeForm.value);
+    const childComponent = this.componentFactoryResolver.resolveComponentFactory(SuccessModalComponent);
+    this.componentRef = this.target.createComponent(childComponent);
   }
+
+
+  resetForm() {
+    this.customizeForm.patchValue({
+      productColor: '',
+      customText: '',
+      textColor: ''
+    });
+    this.customizeForm.reset();
+    this.customizeForm.get('textColor').disable();
+  }
+
 }
