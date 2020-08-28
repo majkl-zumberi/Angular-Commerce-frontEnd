689788ad301cdf288881355751f662b0e0400c37
majkl zumberi <majklzumberi00@gmail.com>
2020-08-28 21:02:12 +0200

    home now only dispatch only a retrieve all clothes action

--- src/app/features/home/components/main/home.component.ts
+++ src/app/features/home/components/main/home.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Store} from '@ngrx/store';
+import {retrieveAllClothes} from '../../../../redux/clothes/clothes.actions';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  constructor(private store: Store) {
+  }
+  ngOnInit(): void {
+    this.store.dispatch(retrieveAllClothes());
+  }
 }
