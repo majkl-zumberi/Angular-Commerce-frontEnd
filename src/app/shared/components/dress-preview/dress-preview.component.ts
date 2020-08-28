a435518a4e94bdd3c0cc12893932ad0e4e25afc8
majkl zumberi <majklzumberi00@gmail.com>
2020-08-28 19:03:15 +0200

    dress-preview.component now emit the active image using element ref instead query selector

--- src/app/shared/components/dress-preview/dress-preview.component.ts
+++ src/app/shared/components/dress-preview/dress-preview.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, EventEmitter, Input, OnDestroy, OnInit, Output, ViewChild} from '@angular/core';
 import Glide from '@glidejs/glide';
 import {Dress} from '../../../core/model/dress.interface';
 @Component({
@@ -6,16 +6,17 @@ import {Dress} from '../../../core/model/dress.interface';
   templateUrl: './dress-preview.component.html',
   styleUrls: ['./dress-preview.component.scss']
 })
-export class DressPreviewComponent implements OnInit, AfterViewInit {
-
+export class DressPreviewComponent implements OnInit, AfterViewInit, OnDestroy {
+  @ViewChild('myDiv') divView: ElementRef;
   @Output()
-  detailEvent: EventEmitter<void> = new EventEmitter();
+  detailEvent: EventEmitter<string> = new EventEmitter();
   @Input()
   dress: Dress;
+  glider: any;
   constructor() { }
 
   ngOnInit(): void {
-
+    this.glider = null;
   }
 
   ngAfterViewInit(): void {
@@ -23,10 +24,24 @@ export class DressPreviewComponent implements OnInit, AfterViewInit {
     // const dressSelector = document.querySelector('.' + this.dress._id);
     const dressSelector = document.getElementById(this.dress._id);
     console.log(dressSelector);
-    new Glide(dressSelector).mount();
+    this.glider = new Glide(dressSelector).mount();
+    this.glider.setActive();
   }
 
   emitDetail() {
-    this.detailEvent.emit();
+    console.log(this.divView);
+    this.divView.nativeElement.firstChild.childNodes.forEach(el => {
+      if (el.className === 'glide__slide glide__slide--active' || el.className === 'glide__slide ng-star-inserted glide__slide--active') {
+        console.log('trovato');
+        console.log(el.firstChild.attributes.src.value);
+        this.detailEvent.emit(el.firstChild.attributes.src.value);
+      }
+      console.log(el.className);
+    });
+  }
+
+  ngOnDestroy(): void {
+    console.log('distruggo');
+    // this.glider.destroy();
   }
 }
