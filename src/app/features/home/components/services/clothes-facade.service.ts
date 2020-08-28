b3bf6bec8e717fcc77acbf5d33adacff3de677a4
majkl zumberi <majklzumberi00@gmail.com>
2020-08-28 19:01:01 +0200

    handled current image from query param

--- src/app/features/home/components/services/clothes-facade.service.ts
+++ src/app/features/home/components/services/clothes-facade.service.ts
@@ -9,7 +9,7 @@ export class ClothesFacadeService {
   constructor(private router: Router) { }
 
   // tslint:disable-next-line:variable-name
-  goToDetail(_id: string) {
-    this.router.navigateByUrl('/home/detail/' + _id);
+  goToDetail(_id: string, currentImage: string) {
+    this.router.navigateByUrl(`/home/detail/${_id}?img=${currentImage}`);
   }
 }
