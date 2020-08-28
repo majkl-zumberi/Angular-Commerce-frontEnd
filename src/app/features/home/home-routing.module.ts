e61d2512980d937be0c6502cef15884c712965d2
majkl zumberi <majklzumberi00@gmail.com>
2020-08-28 19:04:38 +0200

    added clothes route | that component will be the default route for this lazy module

--- src/app/features/home/home-routing.module.ts
+++ src/app/features/home/home-routing.module.ts
@@ -3,10 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './components/main/home.component';
 import {ClotheDetailComponent} from './components/clothe-detail/clothe-detail.component';
+import {ClotheComponent} from './components/clothe/clothe.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent , children: [
-    {path: 'detail/:id', component : ClotheDetailComponent}
-    ]}];
+const routes: Routes = [
+    { path: '', redirectTo: '/home/clothes', pathMatch: 'full' },
+    { path: '', component: HomeComponent , children: [
+    {path: 'clothes', component: ClotheComponent},
+    {path: 'detail/:id', component : ClotheDetailComponent}]
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
