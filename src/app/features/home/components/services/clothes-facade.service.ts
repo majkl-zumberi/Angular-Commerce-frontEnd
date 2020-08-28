f27afb1c4eed6712714b4a0b1155602afd2e1dc9
majkl zumberi <majklzumberi00@gmail.com>
2020-08-28 21:03:36 +0200

    added addToCart method that handle an add to cart action

--- src/app/features/home/components/services/clothes-facade.service.ts
+++ src/app/features/home/components/services/clothes-facade.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@angular/core';
 import {Router} from '@angular/router';
+import {CartProduct} from '../../../../core/model/CartProduct.interface';
+import {Store} from '@ngrx/store';
+import {insertProductToCart} from '../../../../redux/cart/cart.actions';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClothesFacadeService {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private store: Store) { }
 
   // tslint:disable-next-line:variable-name
   goToDetail(_id: string, currentImage: string) {
     this.router.navigateByUrl(`/home/detail/${_id}?img=${currentImage}`);
+  }
+
+  addToCart(cart: CartProduct) {
+    this.store.dispatch(insertProductToCart({cart}));
   }
 }
