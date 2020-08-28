28a25db2f23319985ce5e8c50e3576023a11132e
majkl zumberi <majklzumberi00@gmail.com>
2020-08-28 21:04:19 +0200

    on submit calls facade service for adding a product to the shopping cart

--- src/app/features/home/components/clothe-detail/clothe-detail.component.ts
+++ src/app/features/home/components/clothe-detail/clothe-detail.component.ts
@@ -6,6 +6,8 @@ import {getCurrentNavigatedClothe} from '../../../../redux';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {ActivatedRoute} from '@angular/router';
 import {SuccessModalComponent} from '../success-modal/success-modal.component';
+import {CartProduct} from '../../../../core/model/CartProduct.interface';
+import {ClothesFacadeService} from '../services/clothes-facade.service';
 
 @Component({
   selector: 'app-clothe-detail',
@@ -15,6 +17,7 @@ import {SuccessModalComponent} from '../success-modal/success-modal.component';
 export class ClotheDetailComponent implements OnInit {
   image: string;
   customizeForm: FormGroup;
+  currentDress: Dress;
 
   get clothe(): Observable<Dress> {
     return this.store.pipe(select(getCurrentNavigatedClothe));
@@ -24,12 +27,15 @@ export class ClotheDetailComponent implements OnInit {
 
   constructor(private fb: FormBuilder,
               private store: Store, private route: ActivatedRoute,
-              private componentFactoryResolver: ComponentFactoryResolver) {
+              private componentFactoryResolver: ComponentFactoryResolver,
+              private facadeService: ClothesFacadeService) {
      this.image = this.route.snapshot.queryParamMap.get('img');
      this.customizeForm = this.fb.group({
       productColor: ['', Validators.required],
       customText: [''],
       textColor: [''],
+      currentImage: [''],
+
     });
      this.customizeForm.get('customText').valueChanges.subscribe(text => {
       console.log(text);
@@ -40,6 +46,13 @@ export class ClotheDetailComponent implements OnInit {
       }
     });
      this.customizeForm.get('textColor').disable();
+     this.clothe.subscribe(clothe => {
+       this.currentDress = clothe;
+       console.log(this.currentDress);
+       this.customizeForm.patchValue({
+          currentImage: this.image
+       });
+     });
   }
 
   ngOnInit(): void {
@@ -48,6 +61,13 @@ export class ClotheDetailComponent implements OnInit {
   addToCart() {
     console.log('cliccato');
     console.log(this.customizeForm.value);
+    console.log(this.currentDress);
+    let cart: CartProduct;
+    cart = {
+      ...this.customizeForm.value,
+      prodotto:  {...this.currentDress}
+    };
+    this.facadeService.addToCart(cart);
     const childComponent = this.componentFactoryResolver.resolveComponentFactory(SuccessModalComponent);
     this.componentRef = this.target.createComponent(childComponent);
   }
