b6ad17760d0dfd8193369cde147183b1faa63113
majkl zumberi <majklzumberi00@gmail.com>
2020-08-28 12:43:09 +0200

    created ClotheDetailComponent

--- src/app/features/home/home-routing.module.ts
+++ src/app/features/home/home-routing.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './components/main/home.component';
+import {ClotheDetailComponent} from './components/clothe-detail/clothe-detail.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [{ path: '', component: HomeComponent , children: [
+    {path: 'detail/:id', component : ClotheDetailComponent}
+    ]}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -5,10 +5,11 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './components/main/home.component';
 import {SharedModule} from '../../shared/shared.module';
 import {ClothesFacadeService} from './components/services/clothes-facade.service';
+import { ClotheDetailComponent } from './components/clothe-detail/clothe-detail.component';
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, ClotheDetailComponent],
   providers: [ClothesFacadeService],
   imports: [
     CommonModule,
