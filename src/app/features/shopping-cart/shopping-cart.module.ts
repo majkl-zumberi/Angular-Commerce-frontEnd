2bbc20fc0d931762ffa25bd562ccafc862e34472
majkl zumberi <majklzumberi00@gmail.com>
2020-08-29 21:40:58 +0200

    added textBoxModule for @syncfusion input with floating label

--- src/app/features/shopping-cart/shopping-cart.module.ts
+++ src/app/features/shopping-cart/shopping-cart.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { ShoppingCartRoutingModule } from './shopping-cart-routing.module';
 import { ShoppingCartComponent } from './components/main/shopping-cart.component';
 import { FirstStepComponent } from './components/first-step/first-step.component';
+import { SecondStepComponent } from './components/second-step/second-step.component';
 
-
+import {TextBoxModule} from '@syncfusion/ej2-angular-inputs';
 @NgModule({
-  declarations: [ShoppingCartComponent, FirstStepComponent],
+  declarations: [ShoppingCartComponent, FirstStepComponent, SecondStepComponent],
   imports: [
     CommonModule,
-    ShoppingCartRoutingModule
+    ShoppingCartRoutingModule,
+    TextBoxModule
   ]
 })
 export class ShoppingCartModule { }
