1fe15d9765df582a5d05ab5a087767a9cfce7cbb
majkl zumberi <majklzumberi00@gmail.com>
2020-08-29 21:46:10 +0200

    now shopping-cart-routing.module handle multiple routes | with slide animation

--- src/app/features/shopping-cart/shopping-cart-routing.module.ts
+++ src/app/features/shopping-cart/shopping-cart-routing.module.ts
@@ -2,8 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ShoppingCartComponent } from './components/main/shopping-cart.component';
+import {FirstStepComponent} from './components/first-step/first-step.component';
+import {SecondStepComponent} from './components/second-step/second-step.component';
 
-const routes: Routes = [{ path: '', component: ShoppingCartComponent }];
+const routes: Routes = [
+  { path: '', redirectTo: '/cart/first-step', pathMatch: 'full' },
+  { path: '', component: ShoppingCartComponent },
+  { path: 'first-step', component: FirstStepComponent, data: {animation: 'isLeft'} },
+  { path: 'second-step', component: SecondStepComponent , data: {animation: 'isRight'} },
+  ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
