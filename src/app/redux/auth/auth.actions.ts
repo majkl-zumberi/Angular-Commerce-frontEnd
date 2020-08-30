74c11411a05845d27277f8eaf21da88bf5b6feb8
majkl zumberi <majklzumberi00@gmail.com>
2020-08-30 22:08:36 +0200

    added editUser(effect) & update user actions in auth.actions.ts

--- src/app/redux/auth/auth.actions.ts
+++ src/app/redux/auth/auth.actions.ts
@@ -2,7 +2,7 @@ import { createAction, props} from '@ngrx/store';
 import {User} from '../../core/model/user.interface';
 
 export const initUser  = createAction('[User] init',  props<{user: User}>());
-
+export const updateUser = createAction('[User] update', props<{user: User}>());
 // effects
 // login
 export const loginUser = createAction('[Auth] Login',  props<{email: string, password: string}>());
@@ -14,3 +14,5 @@ export const signUpUser = createAction('[Auth] signUp',  props<{email: string, p
 export const signUpUserSuccess = createAction('[Auth] signUp Success', props<{token: string}>());
 export const signUpUserFailure = createAction('[Auth] signUp Failure', props<{error: string}>());
 
+// update
+export const editUser = createAction('[User] edit', props<{user: User}>());
