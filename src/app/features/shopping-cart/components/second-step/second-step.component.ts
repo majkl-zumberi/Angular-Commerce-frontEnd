777b3567bfa562a0bb1dda67b3cd5d2ac2f89e5a
majkl zumberi <majklzumberi00@gmail.com>
2020-08-30 22:10:53 +0200

    now second-step.component handle submit logic | reactive form

--- src/app/features/shopping-cart/components/second-step/second-step.component.ts
+++ src/app/features/shopping-cart/components/second-step/second-step.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import {ShoppingCartFacadeService} from '../services/shopping-cart-facade.service';
+import {select, Store} from '@ngrx/store';
+import {selectUserState} from '../../../../redux';
+import {User} from '../../../../core/model/user.interface';
 
 @Component({
   selector: 'app-second-step',
@@ -6,10 +12,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./second-step.component.scss']
 })
 export class SecondStepComponent implements OnInit {
+  user: User;
+  // from store | ngrx
+  get userInfo() {
+    return this.store.pipe(select(selectUserState));
+  }
 
-  constructor() { }
+  userForm: FormGroup;
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private facadeServer: ShoppingCartFacadeService,
+    private store: Store) {
+    this.userForm = this.fb.group({
+      lastName: ['', Validators.required],
+      firstName: ['', Validators.required],
+      phone: ['', Validators.required],
+      city: ['', Validators.required],
+      cap: ['', Validators.required],
+      address: ['', Validators.required],
+      houseNumber: ['', Validators.required],
+      courier: [''],
+    });
+    this.userInfo.subscribe(user => {
+      this.userForm.patchValue({
+        firstName: user?.user?.firstName,
+        lastName: user?.user?.lastName,
+        phone: user?.user?.phone,
+        city: user?.user?.city,
+        cap: user?.user?.cap,
+        address: user?.user?.address,
+        houseNumber: user?.user?.houseNumber,
+        courier: user?.user?.courier,
+      });
+      // console.log(user);
+      this.user = {...user.user};
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  saveUserInfo($event: any) {
+    const userVal = {
+      lastName : this.user.lastName,
+      firstName : this.user.firstName,
+      phone : this.user.phone,
+      city : this.user.city,
+      cap : this.user.cap,
+      address : this.user.address,
+      houseNumber : this.user.houseNumber,
+      courier : this.user.courier
+    };
+    // console.log(JSON.stringify(this.userForm.value) === JSON.stringify(userVal));
+    if (JSON.stringify(this.userForm.value) !== JSON.stringify(userVal)) {
+      this.facadeServer.updadeUserInfo(this.userForm.value);
+    }
+    if ($event?.submitter?.textContent === 'indietro' || $event.target.attributes.id.nodeValue === 'previous') {
+        this.router.navigateByUrl('/cart/first-step');
+      }
+  }
 }
