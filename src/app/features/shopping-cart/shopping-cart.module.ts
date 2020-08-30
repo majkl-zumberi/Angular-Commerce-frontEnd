004ee9e94dd41c85131840a7da3c00d6f39e1160
majkl zumberi <majklzumberi00@gmail.com>
2020-08-31 00:37:11 +0200

    added dropdownListModule | syncfusion added third component in shopping-cart.module.ts

--- src/app/features/shopping-cart/shopping-cart.module.ts
+++ src/app/features/shopping-cart/shopping-cart.module.ts
@@ -9,13 +9,16 @@ import { SecondStepComponent } from './components/second-step/second-step.compon
 import {TextBoxModule} from '@syncfusion/ej2-angular-inputs';
 import {SharedModule} from '../../shared/shared.module';
 import {ShoppingCartFacadeService} from './components/services/shopping-cart-facade.service';
+import { ThirdStepComponent } from './components/third-step/third-step.component';
+import {DropDownListModule} from '@syncfusion/ej2-angular-dropdowns';
 @NgModule({
-  declarations: [ShoppingCartComponent, FirstStepComponent, SecondStepComponent],
+  declarations: [ShoppingCartComponent, FirstStepComponent, SecondStepComponent, ThirdStepComponent],
     imports: [
         CommonModule,
         ShoppingCartRoutingModule,
         TextBoxModule,
-        SharedModule
+        SharedModule,
+        DropDownListModule
     ],
     providers: [
       ShoppingCartFacadeService
