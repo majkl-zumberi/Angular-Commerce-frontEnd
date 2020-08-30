ef0c707044eedbb99b8510e02f9788b9c8269ef5
majkl zumberi <majklzumberi00@gmail.com>
2020-08-30 16:40:59 +0200

    added provider for facade service

--- src/app/features/shopping-cart/shopping-cart.module.ts
+++ src/app/features/shopping-cart/shopping-cart.module.ts
@@ -7,12 +7,18 @@ import { FirstStepComponent } from './components/first-step/first-step.component
 import { SecondStepComponent } from './components/second-step/second-step.component';
 
 import {TextBoxModule} from '@syncfusion/ej2-angular-inputs';
+import {SharedModule} from '../../shared/shared.module';
+import {ShoppingCartFacadeService} from './components/services/shopping-cart-facade.service';
 @NgModule({
   declarations: [ShoppingCartComponent, FirstStepComponent, SecondStepComponent],
-  imports: [
-    CommonModule,
-    ShoppingCartRoutingModule,
-    TextBoxModule
-  ]
+    imports: [
+        CommonModule,
+        ShoppingCartRoutingModule,
+        TextBoxModule,
+        SharedModule
+    ],
+    providers: [
+      ShoppingCartFacadeService
+    ]
 })
 export class ShoppingCartModule { }
