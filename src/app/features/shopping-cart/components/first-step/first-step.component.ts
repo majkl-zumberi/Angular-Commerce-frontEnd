e7d71a54ea3de63258a1b336f7d1a8ba691a6d68
majkl zumberi <majklzumberi00@gmail.com>
2020-08-30 16:43:05 +0200

    now first-step.component handle the logic for displaying all the products added to the cart by the user and handle remove button login | ngrx

--- src/app/features/shopping-cart/components/first-step/first-step.component.ts
+++ src/app/features/shopping-cart/components/first-step/first-step.component.ts
@@ -1,15 +1,42 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, OnInit } from '@angular/core';
 import {animate, style, transition, trigger} from '@angular/animations';
+import {select, Store} from '@ngrx/store';
+import {getCartProducts, getCartProductsTotalPrice} from '../../../../redux';
+import {Observable} from 'rxjs';
+import {CartProduct} from '../../../../core/model/CartProduct.interface';
+import {ShoppingCartFacadeService} from '../services/shopping-cart-facade.service';
 
 @Component({
   selector: 'app-first-step',
   templateUrl: './first-step.component.html',
-  styleUrls: ['./first-step.component.scss']
+  styleUrls: ['./first-step.component.scss'],
+  animations: [
+    trigger('fade', [
+      transition('void => *', [
+        style({ opacity: 0 }),
+        animate(500, style({opacity: 1}))
+      ]),
+      transition('* => void', [
+        animate(200, style({opacity: 0}))
+      ])
+    ]),
+  ]
 })
 export class FirstStepComponent implements OnInit {
-  constructor() { }
+  get CartProducts(): Observable<CartProduct[]> {
+    return this.store.pipe(select(getCartProducts));
+  }
+
+  get TotalPrice() {
+    return this.store.pipe(select(getCartProductsTotalPrice));
+  }
+
+  constructor(private store: Store, private facadeService: ShoppingCartFacadeService) { }
 
   ngOnInit(): void {
   }
 
+  removeProductFromCart(product: CartProduct) {
+    this.facadeService.removeProductFromCart(product.prodotto._id);
+  }
 }
