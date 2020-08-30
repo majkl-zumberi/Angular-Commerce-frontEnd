00eadd77dccc7f07ea003cdd51030bb96a4c3d91
majkl zumberi <majklzumberi00@gmail.com>
2020-08-30 22:09:01 +0200

    handled updateUser action in auth.reducers.ts

--- src/app/redux/auth/auth.reducers.ts
+++ src/app/redux/auth/auth.reducers.ts
@@ -1,6 +1,6 @@
 import {User} from '../../core/model/user.interface';
 import {Action, createReducer, on} from '@ngrx/store';
-import {initUser, loginUserFailure, signUpUserFailure} from './auth.actions';
+import {initUser, loginUserFailure, signUpUserFailure, updateUser} from './auth.actions';
 
 export interface UserState {
   user: User;
@@ -18,7 +18,7 @@ export const authReducer = createReducer(
   on(initUser, (state, {user}) => ({ ...state, user, errorMessage: null })),
   on(loginUserFailure, (state, {error}) => ({...state, user: null, errorMessage: error})),
   on(signUpUserFailure, (state, {error}) => ({...state, user: null, errorMessage: error})),
-
+  on(updateUser, (state, {user}) => ({...state, user: {...state.user, ...user} }))
 );
 
 export function reducer(state: UserState | undefined, action: Action) {
