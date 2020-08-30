40071af9089bba86fad40aa506a766b0deb227d2
majkl zumberi <majklzumberi00@gmail.com>
2020-08-30 16:33:44 +0200

    added to cart.actions.ts removeProductToCart

--- src/app/redux/cart/cart.actions.ts
+++ src/app/redux/cart/cart.actions.ts
@@ -3,3 +3,4 @@ import {CartProduct} from '../../core/model/CartProduct.interface';
 
 export const initCart = createAction('[cart] store init',  props<{cart: CartProduct[]}>());
 export const insertProductToCart = createAction('[cart-product] store insert', props<{cart: CartProduct}>());
+export const removeProductToCart = createAction('[cart-product] store remove', props<{id: string}>());
