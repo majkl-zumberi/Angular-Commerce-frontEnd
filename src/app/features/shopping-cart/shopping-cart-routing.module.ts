d95623a32c4b356cc6f538da3372f5b2649d0795
majkl zumberi <majklzumberi00@gmail.com>
2020-08-31 00:36:27 +0200

    added third-step in routes

--- src/app/features/shopping-cart/shopping-cart-routing.module.ts
+++ src/app/features/shopping-cart/shopping-cart-routing.module.ts
@@ -4,12 +4,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { ShoppingCartComponent } from './components/main/shopping-cart.component';
 import {FirstStepComponent} from './components/first-step/first-step.component';
 import {SecondStepComponent} from './components/second-step/second-step.component';
+import {ThirdStepComponent} from './components/third-step/third-step.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/cart/first-step', pathMatch: 'full' },
   { path: '', component: ShoppingCartComponent },
   { path: 'first-step', component: FirstStepComponent, data: {animation: 'isLeft'} },
-  { path: 'second-step', component: SecondStepComponent , data: {animation: 'isRight'} },
+  { path: 'second-step', component: SecondStepComponent},
+  { path: 'third-step', component: ThirdStepComponent , data: {animation: 'isRight'} },
   ];
 
 @NgModule({
