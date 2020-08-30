e0067ac9891df4d73fec308b51a4cd388f39d4e3
majkl zumberi <majklzumberi00@gmail.com>
2020-08-30 22:11:45 +0200

    added updateUserInfo method in shopping-cart-facade.service

--- src/app/features/shopping-cart/components/services/shopping-cart-facade.service.ts
+++ src/app/features/shopping-cart/components/services/shopping-cart-facade.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import {Store} from '@ngrx/store';
 import {removeProductToCart} from '../../../../redux/cart/cart.actions';
+import {User} from '../../../../core/model/user.interface';
+import {editUser} from '../../../../redux/auth/auth.actions';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +14,8 @@ export class ShoppingCartFacadeService {
   // tslint:disable-next-line:variable-name
   removeProductFromCart(_id: string) {
     this.store.dispatch(removeProductToCart({id: _id}));
+  }
+  updadeUserInfo(user: User) {
+    this.store.dispatch(editUser({user}));
   }
 }
