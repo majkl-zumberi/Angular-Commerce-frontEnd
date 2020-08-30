0617f7311c32aba225a4f6e7338c2e7d32062399
majkl zumberi <majklzumberi00@gmail.com>
2020-08-30 16:36:46 +0200

    created a dedicated component for success svg | will be used in a shared modal

---
+++ src/app/shared/svg/success/success.component.ts
@@ -0,0 +1,13 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-success',
+  templateUrl: './success.component.svg',
+  styles: [
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class SuccessComponent {
+  constructor() { }
+
+}
