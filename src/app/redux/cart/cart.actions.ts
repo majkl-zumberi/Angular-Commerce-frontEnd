4a56f7fea1789628ccb10f2ec8390429986dbc46
majkl zumberi <majklzumberi00@gmail.com>
2020-08-31 20:13:11 +0200

    added two actions: empty card & perform purchase

--- src/app/redux/cart/cart.actions.ts
+++ src/app/redux/cart/cart.actions.ts
@@ -4,3 +4,7 @@ import {CartProduct} from '../../core/model/CartProduct.interface';
 export const initCart = createAction('[cart] store init',  props<{cart: CartProduct[]}>());
 export const insertProductToCart = createAction('[cart-product] store insert', props<{cart: CartProduct}>());
 export const removeProductToCart = createAction('[cart-product] store remove', props<{id: string}>());
+export const emptyCart = createAction('[cart] remove all');
+// effects
+export const performPurchase = createAction('[cart] perform purchase');
+
