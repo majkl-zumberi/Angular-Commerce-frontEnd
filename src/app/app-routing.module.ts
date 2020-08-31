455a42c8884b4fb3a8e21cb6f1ec71cb49994e7d
majkl zumberi <majklzumberi00@gmail.com>
2020-08-31 21:19:38 +0200

    added guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {CanLoadAuthGuard} from './core/guards/can-load-auth.guard';
+import {CanActivateAuthGuard} from './core/guards/can-activate-auth.guard';
+import {CanActivateCompsGuard} from './core/guards/can-activate-comps.guard';
+import {CanLoadCompsGuard} from './core/guards/can-load-comps.guard';
 
 
 const routes: Routes = [
-  { path: 'auth', loadChildren: () => import('./features/auth/auth.module').then(m => m.AuthModule) },
-  { path: 'home', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'auth',
+    loadChildren: () => import('./features/auth/auth.module').then(m => m.AuthModule),
+    canLoad: [CanLoadAuthGuard],
+    canActivate: [CanActivateAuthGuard]
+  },
+  { path: 'home', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule),
+    canLoad: [CanLoadCompsGuard],
+    canActivate: [CanActivateCompsGuard]
+  },
   // tslint:disable-next-line:max-line-length
-  { path: 'sessionExpired', loadChildren: () => import('./features/session-expired/session-expired.module').then(m => m.SessionExpiredModule) },
-  { path: 'cart', loadChildren: () => import('./features/shopping-cart/shopping-cart.module').then(m => m.ShoppingCartModule) }];
+  { path: 'sessionExpired',
+    loadChildren: () => import('./features/session-expired/session-expired.module').then(m => m.SessionExpiredModule) },
+  { path: 'cart', loadChildren: () => import('./features/shopping-cart/shopping-cart.module').then(m => m.ShoppingCartModule),
+    canLoad: [CanLoadCompsGuard],
+    canActivate: [CanActivateCompsGuard]
+  },
+  {path: '**', redirectTo: '/home', pathMatch: 'full'}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/features/shopping-cart/shopping-cart-routing.module.ts
+++ src/app/features/shopping-cart/shopping-cart-routing.module.ts
@@ -5,13 +5,14 @@ import { ShoppingCartComponent } from './components/main/shopping-cart.component
 import {FirstStepComponent} from './components/first-step/first-step.component';
 import {SecondStepComponent} from './components/second-step/second-step.component';
 import {ThirdStepComponent} from './components/third-step/third-step.component';
+import {CanActivateStepsGuard} from '../../core/guards/can-activate-steps.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: '/cart/first-step', pathMatch: 'full' },
   { path: '', component: ShoppingCartComponent },
   { path: 'first-step', component: FirstStepComponent, data: {animation: 'isLeft'} },
-  { path: 'second-step', component: SecondStepComponent},
-  { path: 'third-step', component: ThirdStepComponent , data: {animation: 'isRight'} },
+  { path: 'second-step', component: SecondStepComponent, canActivate: [CanActivateStepsGuard]},
+  { path: 'third-step', component: ThirdStepComponent , data: {animation: 'isRight'}, canActivate: [CanActivateStepsGuard] },
   ];
 
 @NgModule({
