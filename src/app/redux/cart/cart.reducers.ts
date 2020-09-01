7b18330b210ef662c36e7adea8af2aed64fb0c4d
majkl zumberi <majklzumberi00@gmail.com>
2020-09-01 21:26:14 +0200

    added modal status in Cart state & handled open, close Modal

--- src/app/redux/cart/cart.reducers.ts
+++ src/app/redux/cart/cart.reducers.ts
@@ -1,13 +1,15 @@
 import {Action, createReducer, on} from '@ngrx/store';
-import {emptyCart, insertProductToCart, removeProductToCart} from './cart.actions';
+import {closeModal, emptyCart, insertProductToCart, openModal, removeProductToCart} from './cart.actions';
 import {CartProduct} from '../../core/model/CartProduct.interface';
 
 export interface CartState {
   cart: CartProduct[];
+  modalOpen: boolean;
 }
 
 export const initialState: CartState = {
-  cart: []
+  cart: [],
+  modalOpen: false
 };
 
 export const cartReducer = createReducer(
@@ -15,7 +17,9 @@ export const cartReducer = createReducer(
  // on(initClothes, (state, {clothes}) => ({...state, clothes})),
   on(insertProductToCart, (state, {cart}) => ({ ...state, cart: [...state.cart, cart] })),
   on(removeProductToCart, (state, {id}) => ({ ...state, cart: state.cart.filter(item => item.prodotto._id !== id) })),
-  on(emptyCart, (state) => ({...state, cart: []}))
+  on(emptyCart, (state) => ({...state, cart: []})),
+  on(openModal, (state) => ({...state, modalOpen: true})),
+  on(closeModal, (state) => ({...state, modalOpen: false}))
 );
 
 export function reducer(state: CartState | undefined, action: Action) {
