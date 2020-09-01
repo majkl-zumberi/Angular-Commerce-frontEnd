d61d9eeec76d8dfd173a61d8312404aa4ad5c635
majkl zumberi <majklzumberi00@gmail.com>
2020-09-01 21:25:04 +0200

    added modal actions: open close

--- src/app/redux/cart/cart.actions.ts
+++ src/app/redux/cart/cart.actions.ts
@@ -5,6 +5,8 @@ export const initCart = createAction('[cart] store init',  props<{cart: CartProd
 export const insertProductToCart = createAction('[cart-product] store insert', props<{cart: CartProduct}>());
 export const removeProductToCart = createAction('[cart-product] store remove', props<{id: string}>());
 export const emptyCart = createAction('[cart] remove all');
+export const openModal = createAction('[cart] open modal purchase success');
+export const closeModal = createAction('[cart] close modal purchase success');
 // effects
 export const performPurchase = createAction('[cart] perform purchase');
 
