5cfde0613834846d439e51d0117ed3ba26e828df
majkl zumberi <majklzumberi00@gmail.com>
2020-09-01 21:36:35 +0200

    can-activate-steps.guard.ts now checks for both selectors cart size & modal status | bug fix

--- src/app/core/guards/can-activate-steps.guard.ts
+++ src/app/core/guards/can-activate-steps.guard.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router} from '@angular/router';
 import { Observable } from 'rxjs';
 import {select, Store} from '@ngrx/store';
-import {getSizeCart} from '../../redux';
+import {getModalState, getSizeCart} from '../../redux';
+import {map, withLatestFrom} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -14,12 +15,24 @@ export class CanActivateStepsGuard implements CanActivate {
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     console.log('devo attivarlo?');
-    this.store.pipe(select(getSizeCart)).subscribe(size => {
+
+    this.store.pipe(select(getSizeCart)).pipe(
+      withLatestFrom(this.store.pipe(select(getModalState))),
+      map(([sizeCart, modalOpen]) => {
+        if (!sizeCart && !modalOpen) {
+          console.log('carrello vuoto e modal finale chiusa ');
+          this.router.navigateByUrl('/cart/first-step');
+          return false;
+        }
+        return true;
+      } )
+    ).subscribe();
+      /*.subscribe(size => {
       if (!size) {
         this.router.navigateByUrl('/cart/first-step');
         return false;
       }
-    });
+    });*/
     return true;
   }
 
