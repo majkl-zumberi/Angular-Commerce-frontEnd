fee28d47573dd7384175e14278d64e5cf6eaa99d
majkl zumberi <majklzumberi00@gmail.com>
2020-09-01 21:30:02 +0200

    in session-expired.component when user clicks the button for logging in it removes the session (for guard bug fix purpose)

--- src/app/features/session-expired/session-expired.component.ts
+++ src/app/features/session-expired/session-expired.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-session-expired',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SessionExpiredComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  redoLogin() {
+    sessionStorage.removeItem('utente');
+    this.router.navigateByUrl('/auth/login');
+  }
 }
