7dfcbc82462b794f2514f9eb7a6e15de02b1432f
majkl zumberi <majklzumberi00@gmail.com>
2020-09-05 23:03:05 +0200

    Update home.spec.js

--- cypress/integration/home/home.spec.js
+++ cypress/integration/home/home.spec.js
@@ -2,15 +2,17 @@ context('home', () => {
   before(() => {
     cy.Login();
     cy.url().should('eq','http://localhost:4200/home/clothes')
+    cy.visit('http://localhost:4200/home/clothes');
   });
 
-  it('checks for menu content [navbar]', () => {
+  it.only('checks for menu content [navbar]', () => {
     cy.get('nav').within($navBar =>{
       cy.wrap($navBar).should('be.visible')
+      console.log($navBar);
       cy.wrap($navBar).children().should('have.length',3)
       cy.wrap($navBar).children().eq(1).should('have.text','Welcome to Lillo.it')
     });
-    cy.get('app-menu-link').first().find('a').should('have.text','Home').and('have.attr','href','/home/clothes')
+    cy.get('app-menu-link').first().find('a').should('not.have.text','Home').and('not.have.attr','href','/home/clothes')
     cy.get('app-menu-link').last().find('a').should('have.text','Carrello (0)').and('have.attr','href','/cart')
   });
 
