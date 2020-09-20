9ecb6822d714282cc2baab27d2bcfd0615990947
majkl zumberi <majklzumberi00@gmail.com>
2020-09-20 19:28:24 +0200

    added provider for UiLangService that will run on APP_INITIALIZER

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -1,19 +1,31 @@
 import {APP_INITIALIZER, NgModule} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {UiStyleToggleService} from '../core/services/ui-style-toggle.service';
+import { SelectLanguageComponent } from './components/select-language/select-language.component';
+import {ThemeTogglerComponent} from './components/theme-toggler/theme-toggler.component';
+import { DropDownSettingsComponent } from './components/drop-down-settings/drop-down-settings.component';
+import {UiLangService} from '../core/services/ui-lang.service';
+import {TranslateModule} from '@ngx-translate/core';
 
 export function themeFactory(themeService: UiStyleToggleService) {
   return () => themeService.setThemeOnStart();
 }
+export function langFactory(themeService: UiLangService) {
+  return () => themeService.setLangOnStart();
+}
 
 @NgModule({
-  declarations: [],
+  declarations: [SelectLanguageComponent, ThemeTogglerComponent, DropDownSettingsComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    TranslateModule
   ],
   providers: [
     UiStyleToggleService,
+    UiLangService,
     {provide: APP_INITIALIZER, useFactory: themeFactory, deps: [UiStyleToggleService], multi: true},
-  ]
+    {provide: APP_INITIALIZER, useFactory: langFactory, deps: [UiLangService], multi: true},
+  ],
+  exports: [SelectLanguageComponent, ThemeTogglerComponent, DropDownSettingsComponent]
 })
 export class UiModule { }
