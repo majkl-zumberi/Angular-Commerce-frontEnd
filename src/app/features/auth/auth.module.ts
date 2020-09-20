3c4ef5df12e71961f70abb8942cc8df01f5a1a47
majkl zumberi <majklzumberi00@gmail.com>
2020-09-20 19:21:50 +0200

    added translate module config in auth.module

--- src/app/features/auth/auth.module.ts
+++ src/app/features/auth/auth.module.ts
@@ -6,14 +6,29 @@ import { AuthComponent } from './components/main/auth.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import {SharedModule} from '../../shared/shared.module';
+import {UiModule} from '../../ui/ui.module';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {HttpClient} from '@angular/common/http';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
-  declarations: [AuthComponent, LoginComponent, SignUpComponent],
+declarations: [AuthComponent, LoginComponent, SignUpComponent],
   imports: [
     CommonModule,
     AuthRoutingModule,
-    SharedModule
+    SharedModule,
+    UiModule,
+    TranslateModule.forChild({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
   ]
 })
 export class AuthModule { }
